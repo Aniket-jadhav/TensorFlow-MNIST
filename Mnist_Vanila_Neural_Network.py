3cc5d5037e16e694625f8ae49aefa00128fa8e70
Aniket Jadhav <aniketjadhav5398@gmail.com>
2019-10-14 18:52:39 -0500

    Update Mnist_Vanila_Neural_Network.py

--- Mnist_Vanila_Neural_Network.py
+++ Mnist_Vanila_Neural_Network.py
@@ -11,3 +11,18 @@ mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 ## 3. Check the dimentions of the dataset
+train_images.shape
+
+## 4. Grayscale to binary
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+## 5. Plot some input
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(x_train[i], cmap=plt.cm.binary)
+    plt.xlabel(y_train[i])
+plt.show()
